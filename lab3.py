214ef9e4e6660909de195b5b91bc99517ee19bf4
Ирина Калужская <i.klzhsk@mail.ru>
2024-10-21 13:01:21 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -70,3 +70,37 @@ def pay():
 def success():
     price = request.args.get('price')
     return render_template('/lab3/success.html', price=price)
+
+
+#  стр 42
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    bg_color = request.args.get('bg_color')
+    font_size = request.args.get('font_size')
+    font_style = request.args.get('font_style')
+
+    resp = make_response(render_template('lab3/settings.html', color=color, bg_color=bg_color, font_size=font_size, font_style=font_style))
+
+    if color:
+        resp = make_response(redirect('/lab3/settings'))
+        resp.set_cookie('color', color)
+
+    if bg_color:  
+        resp.set_cookie('bg_color', bg_color)
+
+    if font_size:
+        resp.set_cookie('font_size', font_size)
+
+    if font_style:
+        resp.set_cookie('font_style', font_style)
+        return resp
+
+    color = request.cookies.get('color')
+    bg_color = request.cookies.get('bg_color')
+    font_size = request.cookies.get('font_size', '16')
+    font_style = request.cookies.get('font_style', 'normal')
+
+    resp = make_response(render_template('lab3/settings.html', color=color, bg_color=bg_color, 
+                        font_size=font_size, font_style=font_style))
+    return resp
