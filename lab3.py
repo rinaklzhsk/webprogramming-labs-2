5cc78c9bb1cc9c84c07a9e9045cb1d8a116559da
Ирина Калужская <i.klzhsk@mail.ru>
2024-10-23 14:30:23 +0700

    вывод кук

--- lab3.py
+++ lab3.py
@@ -4,9 +4,10 @@ lab3 = Blueprint('lab3',__name__)
 
 @lab3.route('/lab3/')
 def lab():
-    name = request.cookies.get('name')
+    name = request.cookies.get('name','Аноним')
     name_color = request.cookies.get('name_color')
-    return render_template('lab3/lab3.html', name=name, name_color=name_color)
+    age = request.cookies.get('age', 'неизвестный')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color, age=age)
 
 
 @lab3.route('/lab3/cookie')
