30712de40edd5f2867a6c21fb2ad803c4282c89a
Ирина Калужская <i.klzhsk@mail.ru>
2024-11-04 22:43:42 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -109,6 +109,7 @@ def power():
 
     
 tree_count = 0
+
 @lab4.route('/lab4/tree', methods = ['GET', 'POST'])
 def tree():
     global tree_count
@@ -125,3 +126,16 @@ def tree():
     return redirect('/lab4/tree')
 
 
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab4/login.html', authorized=False)
+
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login == 'alex' and password == '123':
+        return render_template('/lab4/login.html', login=login, authorized=True)
+
+    error = 'Неверные логин и/или пароль'
+    return render_template('lab4/login.html', error=error, authorized=False)
