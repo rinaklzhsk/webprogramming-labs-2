885412a8e4bea424ff496496e379c1660cd23085
Ирина Калужская <i.klzhsk@mail.ru>
2024-11-04 23:02:08 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -126,6 +126,13 @@ def tree():
     return redirect('/lab4/tree')
 
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'rina', 'password': '1912'},
+    {'login': 'anjela', 'password': '1401'}
+]
+
 @lab4.route('/lab4/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -134,8 +141,12 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('/lab4/login.html', login=login, authorized=True)
+    for user in users: 
+        if login == user['login'] and password == user['password']:
+            return render_template('/lab4/login.html', login=login, authorized=True)
 
     error = 'Неверные логин и/или пароль'
     return render_template('lab4/login.html', error=error, authorized=False)
+
+
+
