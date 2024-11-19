67af440009beeedbac888423a5fbd897cb8c658b
Ирина Калужская <i.klzhsk@mail.ru>
2024-11-19 20:20:14 +0700

    Улучшения

--- lab5.py
+++ lab5.py
@@ -9,6 +9,22 @@ def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
 
+def db_connect():
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'irina_kaluzhskaya_knowledge_base',
+        user = 'irina_kaluzhskaya_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+    return conn, cur
+
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -20,26 +36,17 @@ def register():
     if not login or not password:
         return render_template('lab5/register.html', error='Заполните все поля')
 
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'irina_kaluzhskaya_knowledge_base',
-        user = 'irina_kaluzhskaya_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor()
+    conn, cur = db_connect
 
     cur.execute(f"SELECT login FROM users WHERE login='{login}';")
     if cur.fetchone():
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/register.html', 
                                 error='Такой пользователь уже существует')
 
     cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}','{password}');")
-    conn.commit() # сохранение данных
-    # закрыть соединение с БД
-    cur.close() 
-    conn.close()
+    
+    db_close(conn, cur)
     return render_template('lab5/register.html', login=login)
 
 
