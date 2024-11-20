ec628446dd9abedc492b1a761ffc9208a4d4491f
Ирина Калужская <i.klzhsk@mail.ru>
2024-11-20 14:17:20 +0700

    Заготовка к 6-ой лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 
@@ -17,6 +18,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 
 @app.errorhandler(404)
@@ -92,6 +94,7 @@ def index():
                 <li><a href="''' + url_for('lab3.lab') + '''">Третья лабораторная</a></li>
                 <li><a href="''' + url_for('lab4.lab') + '''">Четвертая лабораторная</a></li>
                 <li><a href="''' + url_for('lab5.lab') + '''">Пятая лабораторная</a></li>
+                <li><a href="''' + url_for('lab6.main') + '''">Шестая лабораторная</a></li>
             </ul>
             
             <footer style = "bottom: 0; position: fixed">
