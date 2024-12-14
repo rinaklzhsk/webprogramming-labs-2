9926848217f5dd49bb49059cbad3ae54755e02d2
Ирина Калужская <i.klzhsk@mail.ru>
2024-12-15 01:13:21 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
 from db.models import users, articles
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, current_user
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -33,3 +34,27 @@ def register():
     return redirect('/lab8/')
 
     
+@lab8.route('/lab8/login/', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            return redirect('/lab8/')
+
+    return render_template('/lab8/login.html', 
+                            error = 'Ошибка входа: логин и/или пароль неверны')
+
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "Список статей"
+
